8b1584b0fe6522ff4fea8c736e45b49c4430a9df
Aviran Bachar <aviranbac1@gmail.com>
2023-01-13 23:23:07 +0200

    Feat(Client & Server): add boilerplate for initial NodeJS & react-native expo apps with TypeScript, add MongoDB connection (need to replace localhost in the future)

---
+++ client/src/App.tsx
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import React, { FC } from "react";
+
+const App: FC = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default App;
---
+++ server/src/server/server.ts
@@ -0,0 +1,11 @@
+import { initMongoConnection } from "../db/mongo-connection";
+import { app } from "./app";
+
+export const initializeApplication: () => Promise<void> = async () => {
+    await initMongoConnection();
+
+    const port = 8080;
+    app.listen(port, () => {
+        console.log(`Server listening on port ${port}!`);
+    });
+};
