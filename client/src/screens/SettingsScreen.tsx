4ce22a28c8483b28f369b305e71afa0f1b1f1345
Aviran Bachar <aviranbac1@gmail.com>
2023-03-26 00:26:02 +0300

    removing components/screens/utils (broken)

--- client/src/screens/SettingsScreen.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from "react-native";
-
-export const SettingsScreen = () => {
-    return (
-        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-            <Text>Settings!</Text>
-        </View>
-    );
-}
--- client/src/utils/navigation-stack.ts
+++
@@ -1,7 +0,0 @@
-import { createStackNavigator } from "@react-navigation/stack";
-
-export type RootStackParamList = {
-    Login: {},
-    Register: {}
-};
-export const Stack = createStackNavigator<RootStackParamList>();
