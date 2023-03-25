11f42aaaef6535e809ff4ae184b5006871806fbd
Aviran Bachar <48032069+AviranBac@users.noreply.github.com>
2023-03-25 23:57:42 +0300

    Delete client/src/Screens directory

--- client/src/Screens/CommonlyUsedScreen.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from "react-native";
-
-export const CommonlyUsedScreen = () => {
-    return (
-        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-            <Text>Recent!</Text>
-        </View>
-    )
-}
