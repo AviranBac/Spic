b14e3930bfdd7d705693b2fea338358e278699a2
Aviran Bachar <aviranbac1@gmail.com>
2023-03-25 14:03:09 +0300

    feat(Database): use DB_USERNAME, DB_PASSWORD and DB_SCHEMA_NAME environment variables

--- server/src/db/mongo-connection.ts
+++ server/src/db/mongo-connection.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
 export const initMongoConnection: () => Promise<void> = async () => {
-    await mongoose.connect('mongodb://127.0.0.1:27017/spic');
+    await mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.q1n7xhk.mongodb.net/${process.env.DB_SCHEMA_NAME}?retryWrites=true&w=majority`);
     console.log('Connected to MongoDB successfully');
 };
--- server/src/types/environment.d.ts
+++ server/src/types/environment.d.ts
@@ -3,6 +3,9 @@ export {};
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
+        DB_USERNAME: string;
+        DB_PASSWORD: string;
+        DB_SCHEMA_NAME: string;
         ACCESS_TOKEN_SECRET: string;
         REFRESH_TOKEN_SECRET: string;
         ACCESS_TOKEN_EXPIRATION_IN_HOURS: string;
