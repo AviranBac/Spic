e28110871390338d1a4977208817c763f735d508
Aviran Bachar <aviranbac1@gmail.com>
2023-03-30 21:29:25 +0300

    style(Store): breakline

--- client/src/store/audio/audio.selectors.ts
+++ client/src/store/audio/audio.selectors.ts
@@ -2,8 +2,7 @@ import { createSelector } from "@reduxjs/toolkit";
 import { RootState } from "../store";
 import { AudioState } from "./audio.model";
 
-const selectAudioState: (state: RootState) => AudioState
-    = (state: RootState) => state.audio;
+const selectAudioState: (state: RootState) => AudioState = (state: RootState) => state.audio;
 
 export const selectSound = createSelector(
     [selectAudioState],
