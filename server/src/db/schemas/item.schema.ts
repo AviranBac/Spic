fc567c6bce4cacbc4dc3a7fd61880e456587bc36
amittes <amitt886@gmail.com>
2023-04-02 22:53:45 +0300

    editing items schema

--- server/src/db/schemas/item.schema.ts
+++ server/src/db/schemas/item.schema.ts
@@ -1,16 +1,22 @@
 import mongoose from 'mongoose';
+import { CategoryModel } from "./category.schema";
 
 const Schema = mongoose.Schema;
 
-export interface Item{
-    id?: mongoose.ObjectId,
+export interface Item {
+    id?: mongoose.Types.ObjectId,
     name: string,
-    imageUrl: string
+    imageUrl: string,
+    categoryId: mongoose.Types.ObjectId
 }
 
 export const ItemModelSchema = new Schema<Item>({
     name: String,
-    imageUrl: String
+    imageUrl: String,
+    categoryId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: CategoryModel.modelName
+    }
 });
 
 export const ItemModel = mongoose.model<Item>("Item", ItemModelSchema);
