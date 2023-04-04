037ae87b3ad91511a6d2e5f124d7d23620536844
amittes <amitt886@gmail.com>
2023-04-04 19:11:32 +0300

    adding route - items by category

--- server/src/db/dal/items.dal.ts
+++ server/src/db/dal/items.dal.ts
@@ -3,3 +3,8 @@ import { Item, ItemModel } from "../schemas/item.schema";
 export const getAllItems = async (): Promise<Item[]> => ItemModel.find();
 
 export const addItem = async (item: Item): Promise<Item> => ItemModel.create(item);
+
+export const getItemsByCategoryId = async (categoryId: string): Promise<Item[]> => {
+    const items = await ItemModel.find({ categoryId });
+    return items;
+  };
--- server/src/routes/items.routes.ts
+++ server/src/routes/items.routes.ts
@@ -1,25 +1,23 @@
-import { authenticate } from "../auth/auth-middleware";
+import { AuthenticatedRequest, authenticate } from "../auth/auth-middleware";
 import { Request, Response, Router } from "express";
-import { getAllItems } from "../db/dal/items.dal";
+import { getItemsByCategoryId } from "../db/dal/items.dal";
 import HttpStatus, { StatusCodes } from "http-status-codes";
 import { Item } from "../db/schemas/item.schema";
 
 const router = Router();
 
-router.get('/:categoryId/:email', authenticate, async (req: Request, res:Response) => {
-    const {categoryId, email} = req.params;
-
+router.get('/:categoryId/', authenticate, async (req: Request, res:Response) => {
+    const categoryId = req.params.categoryId;
+    const userId = (req as AuthenticatedRequest).token;
     let response: Item[] | string;
     let statusCode = StatusCodes.OK;
 
-    try {
-        
-        // TODO: logic for items in a specific category, including common and user specific items
-        response = await getAllItems();
-        console.log(`Sending ${response.length} items. categoryId: ${categoryId}, email: ${email}`);
+    try {        
+        response = await getItemsByCategoryId(categoryId);
+        console.log(`Sending ${response.length} items. categoryId: ${categoryId}, userId: ${userId}`);
     } catch (error) {
         statusCode = HttpStatus.INTERNAL_SERVER_ERROR;
-        response = `Failed while trying to get items. categoryId: ${categoryId}, email: ${email}. Error: ${error}`;
+        response = `Failed while trying to get items. categoryId: ${categoryId}, userId: ${userId}. Error: ${error}`;
         console.log(response);
     }
 
