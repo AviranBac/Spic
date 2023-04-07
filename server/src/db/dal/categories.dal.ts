b6cf9c4718017e4fb5830e206a7aebadc1d68b7c
Aviran Bachar <aviranbac1@gmail.com>
2023-04-07 17:20:58 +0300

    refactor(Init items): reuse a lot of code in init script and make some logs more informative

--- server/src/db/dal/categories.dal.ts
+++ server/src/db/dal/categories.dal.ts
@@ -1,22 +1,6 @@
 import { Category, CategoryModel } from "../schemas/category.schema";
-import mongoose from 'mongoose';
 
 export const getAllCategories = async (): Promise<Category[]> => CategoryModel.find();
 
 export const addCategory = async (category: Category): Promise<Category> => CategoryModel.create(category);
-
-export const getCategoriesMap = async (): Promise<Map<string, mongoose.Types.ObjectId>> => {
-  try {
-    const categories: Category[] = await CategoryModel.find().exec();
-    const categoriesMap: Map<string, mongoose.Types.ObjectId> = new Map();
-    categories.forEach((category: Category) => {
-      categoriesMap.set(category.name, category.id!);
-    });
-    return categoriesMap;
-  } catch (error) {
-    console.log(`Failed while fetching categories: ${error}`);
-    return new Map();
-  }
-};
-
   
