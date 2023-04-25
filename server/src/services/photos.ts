86ea44c29da0f9b10de5ee952ad96ef106c61a32
amittes <amitt886@gmail.com>
2023-04-25 16:20:08 +0300

    feat: more api keys

--- server/src/services/photos.ts
+++ server/src/services/photos.ts
@@ -2,15 +2,27 @@ import { ContentFilter, createApi, Language } from 'unsplash-js';
 import * as PhotoApi from "unsplash-js/dist/methods/photos/types";
 import fetch from "node-fetch";
 
-const unsplash = createApi({
-  accessKey: process.env.UNSPLASH_API_KEY || '',
-  fetch
+const keys = [
+  process.env.UNSPLASH_API_KEY_1 || '',
+  process.env.UNSPLASH_API_KEY_2 || '',
+  process.env.UNSPLASH_API_KEY_3 || '',
+  process.env.UNSPLASH_API_KEY_4 || '',
+  process.env.UNSPLASH_API_KEY_5 || ''
+];
+
+let currentKeyIndex = 0;
+
+const unsplashInstances = keys.map((key) => {
+  return createApi({
+    accessKey: key,
+    fetch
+  });
 });
 
 const getPhotos = async (searchQuery: string): Promise<string[]> => {
   let photos: string[] = [];
   try {
-    const response = await unsplash.search.getPhotos({
+    const response = await unsplashInstances[currentKeyIndex].search.getPhotos({
       query: searchQuery,
       perPage: 10,
       contentFilter: 'high' as ContentFilter,
@@ -21,7 +33,14 @@ const getPhotos = async (searchQuery: string): Promise<string[]> => {
     console.log(`Got ${photos.length} photos from Unsplash API, searchQuery: ${searchQuery}`);
   } catch (error) {
     console.log(`Failed while fetching photos with searchQuery ${searchQuery}. Error: ${JSON.stringify(error)}`);
+    currentKeyIndex = (currentKeyIndex + 1) % keys.length;
+    console.log(`Switched to Unsplash API key ${currentKeyIndex}`);
+    return getPhotos(searchQuery);
   }
+
+  currentKeyIndex = (currentKeyIndex + 1) % keys.length;
+  console.log(`Switched to Unsplash API key ${currentKeyIndex}`);
+
   return photos;
 };
 
