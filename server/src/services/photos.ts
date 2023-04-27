2f744a6ac7d236105781317dfb5b57b6d2951958
Aviran Bachar <aviranbac1@gmail.com>
2023-04-27 16:39:00 +0300

    fix(Unsplash retries): refactoring Unsplash API retries with multiple apiKeys

--- server/src/services/photos.ts
+++ server/src/services/photos.ts
@@ -2,21 +2,24 @@ import { ContentFilter, createApi, Language } from 'unsplash-js';
 import * as PhotoApi from "unsplash-js/dist/methods/photos/types";
 import fetch from "node-fetch";
 
-const apiKeys = process.env.UNSPLASH_API_KEYS?.split(',') ?? [];
-
+const apiKeys = process.env.UNSPLASH_API_KEYS.split(',');
 let currentKeyIndex = 0;
 
-const unsplashInstances = apiKeys.map((key) => {
-  return createApi({
-    accessKey: key,
-    fetch
-  });
-});
+const unsplashInstances = apiKeys.map((accessKey: string) => createApi({accessKey, fetch}));
 
-const getPhotos = async (searchQuery: string): Promise<string[]> => {
+const getPhotos = async (searchQuery: string, keyIndex = currentKeyIndex, retryNumber = 0): Promise<string[]> => {
+  if (retryNumber === apiKeys.length) {
+    currentKeyIndex = (keyIndex + 1) % apiKeys.length;
+    throw new Error('All Unsplash API keys failed');
+  }
+
+  if (retryNumber !== 0) {
+    console.log(`Trying another Unsplash API key, index: ${keyIndex}, total retry number: ${retryNumber}`);
+  }
+
   let photos: string[] = [];
   try {
-    const response = await unsplashInstances[currentKeyIndex].search.getPhotos({
+    const response = await unsplashInstances[keyIndex].search.getPhotos({
       query: searchQuery,
       perPage: 10,
       contentFilter: 'high' as ContentFilter,
@@ -27,16 +30,11 @@ const getPhotos = async (searchQuery: string): Promise<string[]> => {
     console.log(`Got ${photos.length} photos from Unsplash API, searchQuery: ${searchQuery}`);
   } catch (error) {
     console.log(`Failed while fetching photos with searchQuery ${searchQuery}. Error: ${JSON.stringify(error)}`);
-    currentKeyIndex = (currentKeyIndex + 1) % apiKeys.length;
-    console.log(`Switched to Unsplash API key ${currentKeyIndex}`);
-    if (currentKeyIndex === 0) {
-      throw new Error('All Unsplash API keys failed');
-    }
-    return getPhotos(searchQuery);
+    return getPhotos(searchQuery, (keyIndex + 1) % apiKeys.length, retryNumber + 1);
   }
 
-  currentKeyIndex = (currentKeyIndex + 1) % apiKeys.length;
-  console.log(`Switched to Unsplash API key ${currentKeyIndex}`);
+  currentKeyIndex = (keyIndex + 1) % apiKeys.length;
+  console.log(`Switched current Unsplash API key to index ${currentKeyIndex}`);
 
   return photos;
 };
