328cb47d87cc83003e6eb841f8a6d48f4950240c
MorRefael <morefael28@gmail.com>
2023-05-27 17:20:41 +0300

    add message when user details fails & hide expo notification

--- client/src/config/config.ts
+++ client/src/config/config.ts
@@ -3,5 +3,5 @@ interface Config {
 }
 
 export const config: Config = {
-    serverUrl: 'http://192.168.1.246:8080/' // TODO: change to your local IP address so it'll work in mobile as well
+    serverUrl: 'http://192.168.1.152:8080' // TODO: change to your local IP address so it'll work in mobile as well
 }
